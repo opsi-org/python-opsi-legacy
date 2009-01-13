235f540c1980c7cb954655ed4610cb0561a66605
Jan Schneider <j.schneider@uib.de>
2009-01-13 12:18:25 +0000

    release of 3.3.1.0-1

--- src/OPSI/Backend/File.py
+++ src/OPSI/Backend/File.py
@@ -32,7 +32,7 @@
    @license: GNU General Public License version 2
 """
 
-__version__ = '0.9.7.4'
+__version__ = '0.9.7.6'
 
 # Imports
 import socket, os, time, re, ConfigParser, json, StringIO, codecs
@@ -943,7 +943,8 @@ class FileBackend(File, DataBackend):
 			"hostId": 	hostId,
 			"description":	"",
 			"notes":	"",
-			"lastSeen":	"" }
+			"lastSeen":	"",
+			"created":	"" }
 		
 		ini = self.readIniFile( os.path.join(self.__pcpatchDir, self.getIniFile(hostId)) )
 		
@@ -993,7 +994,7 @@ class FileBackend(File, DataBackend):
 				logger.info("Host lookup not yet implemented for this OS")
 		return info
 		
-	def getClients_listOfHashes(self, serverId=None, depotId=None, groupId=None, productId=None, installationStatus=None, actionRequest=None, productVersion=None, packageVersion=None):
+	def getClients_listOfHashes(self, serverId=None, depotIds=[], groupId=None, productId=None, installationStatus=None, actionRequest=None, productVersion=None, packageVersion=None):
 		""" Returns a list of client-ids which are connected 
 		    to the server with the specified server-id. 
 		    If no server is specified, all registered clients are returned """
@@ -1101,32 +1102,53 @@ class FileBackend(File, DataBackend):
 			hostIds = filteredHostIds
 		
 		infos = []
+		defaultDepotId = self.getDepotId()
 		for hostId in hostIds:
 			try:
-				infos.append( self.getHost_hash(hostId) )
+				info = self.getHost_hash(hostId)
+				info['depotId'] = defaultDepotId
+				infos.append(info)
 			except BackendIOError, e:
 				logger.error(e)
 		
 		return infos
 		
 		
-	def getClientIds_list(self, serverId=None, depotId=None, groupId=None, productId=None, installationStatus=None, actionRequest=None, productVersion=None, packageVersion=None):
+	def getClientIds_list(self, serverId=None, depotIds=[], groupId=None, productId=None, installationStatus=None, actionRequest=None, productVersion=None, packageVersion=None):
 		clientIds = []
-		for info in self.getClients_listOfHashes(serverId, depotId, groupId, productId, installationStatus, actionRequest, productVersion, packageVersion):
+		for info in self.getClients_listOfHashes(serverId, depotIds, groupId, productId, installationStatus, actionRequest, productVersion, packageVersion):
 			clientIds.append( info.get('hostId') )
 		return clientIds
 
 	def getServerIds_list(self):
-		return [ self.getServerId()  ]
+		return [ self.getServerId() ]
 	
 	def getServerId(self, clientId=None):
 		# Return hostid of localhost
 		return self.getHostId(socket.gethostname())
 
 	def getDepotIds_list(self):
-		return []
+		return self.getServerIds_list()
 		
 	def getDepotId(self, clientId=None):
+		return self.getServerId()
+	
+	def getDepot_hash(self, depotId):
+		depotId = self._preProcessHostId(depotId)
+		return {
+			"description" : "",
+			"notes" : "",
+			"network" : "0.0.0.0/0",
+			"depotLocalUrl" : "file:///opt/pcbin/install",
+			"depotRemoteUrl" : "smb://%s/opt_pcbin/install" % depotId.split('.')[0],
+			"repositoryLocalUrl" : "file:///var/lib/opsi/products",
+			"repositoryRemoteUrl" : "webdavs://%s:4447/products" % depotId,
+			"repositoryMaxBandwidth" : 0
+		}
+	
+	def deleteDepot(self, depotId):
+		depotId = self._preProcessHostId(depotId)
+		logger.error("Cannot delete depot '%s': not implemented in File backend" % depotId)
 		return
 	
 	def getOpsiHostKey(self, hostId):
@@ -1338,6 +1360,14 @@ class FileBackend(File, DataBackend):
 	# -------------------------------------------------
 	# -     PRODUCT FUNCTIONS                         -
 	# -------------------------------------------------
+	def lockProduct(self, productId, depotIds=[]):
+		return
+	
+	def unlockProduct(self, productId, depotIds=[]):
+		return
+	
+	def getProductLocks_hash(self, depotIds=[]):
+		return {}
 	
 	def createProduct(self, productType, productId, name, productVersion, packageVersion, licenseRequired=0,
 			   setupScript="", uninstallScript="", updateScript="", alwaysScript="", onceScript="",
@@ -2458,7 +2488,7 @@ class FileBackend(File, DataBackend):
 	
 	def getProductProperties_hash(self, productId, objectId = None):
 		
-		if not objectId:
+		if not objectId or (objectId.lower() == self.getDepotId().lower()):
 			objectId = self._defaultDomain
 		
 		productId = productId.lower()
@@ -2517,12 +2547,15 @@ class FileBackend(File, DataBackend):
 		
 	def setProductProperties(self, productId, properties, objectId = None):
 		
+		if not objectId or (objectId.lower() == self.getDepotId().lower()):
+			objectId = self._defaultDomain
+		
 		productId = productId.lower()
 		
 		if productId in self.getProductIds_list('netboot'):
 			# The product is a net-boot product
 			iniFile = None
-			if (not objectId or objectId == self._defaultDomain):
+			if (objectId == self._defaultDomain):
 				iniFile = os.path.join(self.__opsiTFTPDir, "global.sysconf")
 			else:
 				iniFile = os.path.join(self.__opsiTFTPDir, self.getSysconfFile(objectId))
@@ -2555,7 +2588,7 @@ class FileBackend(File, DataBackend):
 		# The product is a local-boot product
 		setIniFiles = []
 		iniFiles = []
-		if (not objectId or objectId == self._defaultDomain):
+		if (objectId == self._defaultDomain):
 			# No specific client selected => change config files of all clients and prototype file
 			setIniFiles = [ os.path.join(self.__pcpatchDir, "pcproto.ini") ]
 			for clientId in self.getClientIds_list():
@@ -2620,6 +2653,10 @@ class FileBackend(File, DataBackend):
 			raise BackendIOError( ', '.join(errorList) )
 	
 	def deleteProductProperty(self, productId, property, objectId = None):
+		
+		if not objectId or (objectId.lower() == self.getDepotId().lower()):
+			objectId = self._defaultDomain
+		
 		productId = productId.lower()
 		
 		iniFiles = []
@@ -2627,7 +2664,7 @@ class FileBackend(File, DataBackend):
 		
 		if productId in self.getProductIds_list('netboot'):
 			# The product is a net-boot product
-			if (not objectId or objectId == self._defaultDomain):
+			if (objectId == self._defaultDomain):
 				iniFiles = [ os.path.join(self.__opsiTFTPDir, "global.sysconf") ]
 				for clientId in self.getClientIds_list():
 					iniFiles.append( os.path.join(self.__opsiTFTPDir, self.getSysconfFile(clientId)) )
@@ -2657,7 +2694,7 @@ class FileBackend(File, DataBackend):
 			
 		else:
 			# The product is a local-boot product
-			if (not objectId or objectId == self._defaultDomain):
+			if (objectId == self._defaultDomain):
 				# No specific client selected => change config files of all clients and prototype file
 				iniFiles = [ os.path.join(self.__pcpatchDir, "pcproto.ini") ]
 				for clientId in self.getClientIds_list():
@@ -2691,10 +2728,13 @@ class FileBackend(File, DataBackend):
 		
 		if ( len(errorList) > 0 ):
 			# One or more errors occured => raise esception
-			raise BackendIOError( ', '.join(errorList) )	
+			raise BackendIOError( ', '.join(errorList) )
 		
 	
 	def deleteProductProperties(self, productId, objectId = None):
+		if not objectId or (objectId.lower() == self.getDepotId().lower()):
+			objectId = self._defaultDomain
+		
 		productId = productId.lower()
 		
 		iniFiles = []
@@ -2702,7 +2742,7 @@ class FileBackend(File, DataBackend):
 		
 		if productId in self.getProductIds_list('netboot'):
 			# The product is a net-boot product
-			if (not objectId or objectId == self._defaultDomain):
+			if (objectId == self._defaultDomain):
 				iniFiles = [ os.path.join(self.__opsiTFTPDir, "global.sysconf") ]
 				for clientId in self.getClientIds_list():
 					iniFiles.append( os.path.join(self.__opsiTFTPDir, self.getSysconfFile(clientId)) )
@@ -2738,7 +2778,7 @@ class FileBackend(File, DataBackend):
 			
 		else:
 			# The product is a local-boot product
-			if (not objectId or objectId == self._defaultDomain):
+			if (objectId == self._defaultDomain):
 				# No specific client selected => change config files of all clients and prototype file
 				iniFiles = [ os.path.join(self.__pcpatchDir, "pcproto.ini") ]
 				for clientId in self.getClientIds_list():
