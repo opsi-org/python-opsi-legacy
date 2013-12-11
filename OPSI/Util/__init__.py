30847b09e014e23a2e3956fc0e349af52fdacece
Erol Ülükmen <e.ueluekmen@uib.de>
2013-12-11 14:58:45 +0000

    tagged new version 4.0.4.2-1, inherits optimization of objectToBeatifiedText Method from Utils

--- OPSI/Util/__init__.py
+++ OPSI/Util/__init__.py
@@ -272,40 +272,39 @@ def timestamp(secs=0, dateOnly=False):
 	else:
 		return time.strftime( u"%Y-%m-%d %H:%M:%S", time.localtime(secs) )
 
-
-def objectToBeautifiedText(obj, level=0):
+def objectToBeautifiedText(obj,level=0):
 	if (level == 0):
 		obj = serialize(obj)
 
 	hspace = level*10
 	text = u''
 	if type(obj) is types.ListType:
-		text += u' '*hspace + u'[ \n'
+		text = u'%s%s[ \n' % (text, u' '*hspace)
 		for i in range( len(obj) ):
 			if not type(obj[i]) in (types.DictType, types.ListType):
-				text += u' '*hspace
-			text += objectToBeautifiedText(obj[i], level+1)
+				text = u'%s%s' % (text, u' '*hspace)
+			text = u'%s%s' % (text, objectToBeautifiedText(obj[i], level+1))
 
 			if (i < len(obj)-1):
-				text += u',\n'
-		text += u'\n' + u' '*hspace + u']'
+				text = u"%s,\n" % (text)
+		text = u'%s\n%s]' % (text, u' '*hspace)
 	elif type(obj) is types.DictType:
-		text += u' '*hspace + u'{ \n'
+		text = u'%s%s{ \n' % (text, u' '*hspace)
 		i = 0
 		for (key, value) in obj.items():
-			text += u' '*hspace + u'"' + key + u'" : '
+			text = u'%s%s%s" : ' % (text, u' '*hspace, key)
 			if type(value) in (types.DictType, types.ListType):
-				text += u'\n'
-			text += objectToBeautifiedText(value, level+1)
+				text = u'%s\n' % text
+			text = u'%s%s' % (text, objectToBeautifiedText(value, level+1))
 
 			if (i < len(obj)-1):
-				text += u',\n'
+				text = u'%s,\n' % text
 			i+=1
-		text += u'\n' + u' '*hspace + u'}'
+		text = u'%s\n%s}' % (text, u' '*hspace)
 	elif type(obj) is str:
-		text += toJson(forceUnicode(obj))
+		text = u'%s%s' % (text, toJson(forceUnicode(obj)))
 	else:
-		text += toJson(obj)
+		text = u'%s%s' % (text, toJson(obj))
 	return text
 
 
