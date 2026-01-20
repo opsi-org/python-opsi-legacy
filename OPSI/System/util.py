0a966a4231fc0a787b7de6f6a41157b407cf8c49
Jan Schneider <j.schneider@uib.de>
2026-01-20 10:50:06 +0100

    [fix] Fix EFI secure boot certificate parsing and improve logging

--- OPSI/System/util.py
+++ OPSI/System/util.py
@@ -13,9 +13,12 @@ from opsicommon.logging import get_logger
 
 
 def _get_secure_boot_certificates_from_efivar_payload(data: bytes) -> list[x509.Certificate]:
+	logger = get_logger("opsi.general")
 	certs = []
 	offset = 0
-	while offset < len(data):
+	data_len = len(data)
+	logger.debug("Parsing EFI variable payload of size %d bytes", data_len)
+	while offset < data_len:
 		# EFI_SIGNATURE_LIST header
 		# typedef struct _EFI_SIGNATURE_LIST {
 		#   GUID SignatureType
@@ -23,27 +26,36 @@ def _get_secure_boot_certificates_from_efivar_payload(data: bytes) -> list[x509.
 		#   UINT32 SignatureHeaderSize
 		#   UINT32 SignatureSize
 		# } EFI_SIGNATURE_LIST;
-		if offset + 28 > len(data):
+		if offset + 28 > data_len:
+			logger.debug("Reached end of data while looking for EFI_SIGNATURE_LIST header")
 			break
 
 		sig_type_raw, list_size, header_size, sig_size = struct.unpack_from("<16sIII", data, offset)
 		sig_type = UUID(bytes_le=sig_type_raw)
-		if sig_type != UUID("a5c059a1-94e4-4aa7-87b5-ab155c2bf072"):
-			continue
+		if list_size < 28 or offset + list_size > data_len:
+			logger.warning("Invalid EFI_SIGNATURE_LIST size %d at offset %d", list_size, offset)
+			break
 
 		list_end = offset + list_size
+		if sig_type != UUID("a5c059a1-94e4-4aa7-87b5-ab155c2bf072"):
+			logger.debug("Skipping unsupported EFI signature type %s at offset %d", sig_type, offset)
+			offset = list_end
+			continue
+
 		offset = offset + 28 + header_size
 
 		# Parse signature entries
 		while offset + sig_size <= list_end:
 			sig_data = data[offset : offset + sig_size]
 			try:
+				logger.debug("Parsing secure boot certificate at offset %d", offset)
 				certs.append(x509.load_der_x509_certificate(sig_data[16:]))
 			except Exception as err:
-				get_logger("opsi.general").warning("Failed to parse secure boot certificate: %s", err)
+				logger.warning("Failed to parse secure boot certificate: %s", err)
 			offset += sig_size
 
 		# Safety: move to end of the list in case of padding
 		offset = list_end
 
+	logger.debug("Parsed %d secure boot certificates", len(certs))
 	return certs
