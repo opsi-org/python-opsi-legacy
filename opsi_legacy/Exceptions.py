8d0c6c6d1715df37d79ec3619788fba04ac9a6c3
Jan Schneider <j.schneider@uib.de>
2026-04-27 19:15:00 +0200

    [fix] Add missing exceptions to opsi_legacy.Exceptions

--- opsi_legacy/Exceptions.py
+++ opsi_legacy/Exceptions.py
@@ -8,7 +8,36 @@ OPSI Exceptions.
 Deprecated, use opsi.exception instead.
 """
 
-from opsi.exception import *  # noqa: F403
+from opsi.exception import (  # noqa: F401
+	BackendAuthenticationError,
+	BackendBadValueError,
+	BackendConfigurationError,
+	BackendError,
+	BackendIOError,
+	BackendMissingDataError,
+	BackendModuleDisabledError,
+	BackendPermissionDeniedError,
+	BackendReferentialIntegrityError,
+	BackendTemporaryError,
+	BackendUnableToConnectError,
+	BackendUnaccomplishableError,
+	OperatingSystemUnsupportedError,
+	OpsiBadRpcError,
+	OpsiError,
+	OpsiLicenseConfigurationError,
+	OpsiLicenseMissingError,
+	OpsiRepositoryError,
+	OpsiRpcError,
+	OpsiServiceAuthenticationError,
+	OpsiServiceClientCertificateError,
+	OpsiServiceConnectionError,
+	OpsiServiceConnectionRefusedError,
+	OpsiServiceError,
+	OpsiServicePermissionError,
+	OpsiServiceTimeoutError,
+	OpsiServiceUnavailableError,
+	OpsiServiceVerificationError,
+)
 
 
 class CommandNotFoundException(RuntimeError):
@@ -17,3 +46,53 @@ class CommandNotFoundException(RuntimeError):
 
 class RepositoryError(OpsiError):
 	pass
+
+
+class OpsiBackupFileError(OpsiError):
+	ExceptionShortDescription = "Opsi backup file error"
+
+
+class OpsiBackupFileNotFound(OpsiBackupFileError):
+	ExceptionShortDescription = "Opsi backup file not found"
+
+
+class OpsiBackupBackendNotFound(OpsiBackupFileError):
+	ExceptionShortDescription = "Opsi backend not found in backup"
+
+
+class OpsiProductOrderingError(OpsiError):
+	ExceptionShortDescription = "A condition for ordering cannot be fulfilled"
+
+	def __init__(self, message: str = "", problematicRequirements: list[int] | list[str] | None = None) -> None:
+		super().__init__(message)
+		self.problematicRequirements: list[int] | list[str] | list = problematicRequirements or []
+
+	def __str__(self) -> str:
+		if self.message:
+			if self.problematicRequirements:
+				return f"{self.ExceptionShortDescription}: {self.message} ({self.problematicRequirements})"
+			return f"{self.ExceptionShortDescription}: {self.message}"
+		return self.ExceptionShortDescription
+
+	def __repr__(self) -> str:
+		if self.message:
+			if self.problematicRequirements:
+				return f'<{self.__class__.__name__}("{self.message}", {self.problematicRequirements})>'
+			return f'<{self.__class__.__name__}("{self.message}")>'
+		return f"<{self.__class__.__name__}>"
+
+
+class LicenseConfigurationError(OpsiError):
+	"""Exception raised if a configuration error occurs in the license data base."""
+
+	ExceptionShortDescription = "License configuration error"
+
+
+class LicenseMissingError(OpsiError):
+	"""Exception raised if a license is requested but cannot be found."""
+
+	ExceptionShortDescription = "License missing error"
+
+
+class CanceledException(Exception):
+	ExceptionShortDescription = "CanceledException"
